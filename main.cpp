3892c23116e647054a11e1d795901402035c44a0
Jonathan Shen <jonathanasdf@hotmail.com>
2016-04-20 01:29:33 -0400

    main.cpp

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <opencv2/opencv.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+
+using namespace cv;
+using namespace std;
+
+int main(int argc, char** argv) {
+
+  return 0;
+}
